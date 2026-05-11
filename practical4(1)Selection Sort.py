de18762f3e902e99307f2f6202110d79672bdfbe
Preity0210 <mestripreity@gmail.com>
2026-05-11 21:14:29 +0530

    Add files via upload

---
+++ practical4(1)Selection Sort.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input("Enter numbers: ").split()))
+
+for i in range(len(arr)):
+    minIndex = i
+
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[minIndex]:
+            minIndex = j
+
+    arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+print("Sorted Array:", arr)
